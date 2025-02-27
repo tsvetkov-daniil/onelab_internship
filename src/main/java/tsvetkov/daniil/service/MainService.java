30e7de4729d042bd97ba8cc8f42851827aaa85d3
tsvetkov-daniil <60396791+tsvetkov-daniil@users.noreply.github.com>
2025-02-27 05:09:40 +0500

    added service

---
+++ src/main/java/tsvetkov/daniil/service/MainService.java
@@ -0,0 +1,79 @@
+package tsvetkov.daniil.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tsvetkov.daniil.Main;
+import tsvetkov.daniil.dto.AuthorDTO;
+import tsvetkov.daniil.dto.BookDTO;
+import tsvetkov.daniil.dto.GenreDTO;
+import tsvetkov.daniil.repository.AuthorRepository;
+import tsvetkov.daniil.repository.BookRepository;
+import tsvetkov.daniil.repository.GenreRepository;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class MainService {
+    private final GenreRepository genreRepository;
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public MainService(GenreRepository gp, AuthorRepository ap, BookRepository bp) {
+        this.genreRepository = gp;
+        this.authorRepository = ap;
+        this.bookRepository = bp;
+    }
+
+    public BookDTO addBook(BookDTO book) {
+        if (Objects.nonNull(book.getAuthors())) {
+            book.getAuthors().forEach(this::addAuthor);
+        }
+
+        if (Objects.nonNull(book.getGenres())) {
+            book.getGenres().forEach(this::addGenre);
+        }
+
+        System.out.println("Saving book...");
+        return bookRepository.save(book);
+    }
+
+    public Optional<BookDTO> getBookById(int id) {
+        return bookRepository.findById(id);
+    }
+
+    public Set<BookDTO> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public boolean removeBookById(int id) {
+        return bookRepository.removeById(id);
+    }
+
+    public GenreDTO addGenre(GenreDTO genre) {
+        if (Objects.nonNull(genre) && genreRepository.findByName(genre.getName()).isEmpty()) {
+            System.out.println("Saving genre...");
+            return genreRepository.save(genre);
+        }
+        return null;
+    }
+
+    public AuthorDTO addAuthor(AuthorDTO author) {
+        if (Objects.nonNull(author) && !authorRepository.isExists(author)) {
+            System.out.println("Saving author...");
+            return authorRepository.save(author);
+        }
+        return null;
+    }
+
+    public Set<AuthorDTO> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Set<GenreDTO> getAllGenres() {
+        return genreRepository.findAll();
+    }
+}
